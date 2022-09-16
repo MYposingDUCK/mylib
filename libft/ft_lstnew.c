0d728883fe3e12d18842e00d4fb0b8d0ea715611
MYposingDUCK <yurfriend.kek@gmail.com>
2022-09-16 09:32:23 -0400

    add ft_lstadd_front.c

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -3,7 +3,8 @@
 t_list  *ft_lstnew(void *content)
 {
 t_list *obj;
-obj = malloc(sizeof(*obj) + sizeof(*content));
+obj = malloc(sizeof(*obj));
 obj->content = content;
+obj->next = NULL;
 return(obj); 
 }
--- libft/libft.h
+++ libft/libft.h
@@ -22,6 +22,8 @@ typedef struct s_list
 }                   t_list;
 
 t_list  *ft_lstnew(void *content);
+void    ft_lstadd_front(t_list **lst, t_list *new);
+int     ft_lstsize(t_list *lst);
 int		ft_strlen(const char *s);
 char	*ft_strdup(char *src);
 size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize);
